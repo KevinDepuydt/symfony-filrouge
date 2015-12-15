0363e314ee6e9e32ef73f3e6aabc77f334cca24c
Kevin Depuydt <kevin.depuydt@supinternet.fr>
2015-12-15 16:03:41 +0100

    Add StofDoctrineExtension and DoctrineFixtures Bundles

---
+++ src/Joli/BlogBundle/DataFixtures/ORM/LoadPostFixtures.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kévin
+ * Date: 11/12/2015
+ * Time: 11:05
+ */
+
+namespace Joli\BlogBundle\ORM\DataFixtures;
+
+use Doctrine\Common\DataFixtures\FixtureInterface,
+    Doctrine\Common\Persistence\ObjectManager,
+    Joli\BlogBundle\Entity\Post;
+
+class LoadPostFixtures implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 100; $i++) {
+            $post = new Post();
+            $post->setTitle(sprintf('Titre du post n°%d', $i))
+                 ->setBody(sprintf('Body du post n°%d', $i))
+                 ->setIsPublished($i%2);
+
+            $manager->persist($post);
+        }
+
+        $manager->flush();
+    }
+}
