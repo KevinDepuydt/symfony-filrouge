68afa90cd85a690c2c81a8c27a5c862c87e66543
Kevin Depuydt <kevin.depuydt@supinternet.fr>
2015-12-15 16:06:06 +0100

    Create PostRepository

---
+++ src/Joli/BlogBundle/Repository/PostRepository.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kévin
+ * Date: 11/12/2015
+ * Time: 10:51
+ */
+
+namespace Joli\BlogBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class PostRepository extends EntityRepository
+{
+    public function findAll()
+    {
+        return $this->getEntityManager()->createQueryBuilder()
+            ->add('select p')
+            ->add('from', 'Post p')
+            ->add('where', 'p.isPublished = :published')
+            ->setParameter('published', true)
+            ->getQuery()
+            ->getResult();
+    }
+}
