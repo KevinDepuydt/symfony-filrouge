762a8c733fb96990c6b01288a222dfd445e29c0e
Kevin Depuydt <kevin.depuydt@supinternet.fr>
2016-02-01 16:25:45 +0100

    Add Pagerfanta to project

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Joli\BlogBundle\JoliBlogBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
