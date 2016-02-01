d46af3928350614ac1956bdf72f261804ca988b0
Kevin Depuydt <kevin.depuydt@supinternet.fr>
2016-02-01 16:26:54 +0100

    Get posts list using pagerfanta

--- src/Joli/BlogBundle/Controller/ListController.php
+++ src/Joli/BlogBundle/Controller/ListController.php
@@ -11,19 +11,45 @@ namespace Joli\BlogBundle\Controller;
 use Joli\BlogBundle\Entity\Post,
     Symfony\Bundle\FrameworkBundle\Controller\Controller,
     Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
-    Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+    Sensio\Bundle\FrameworkExtraBundle\Configuration\Template,
+    Pagerfanta\Adapter\DoctrineORMAdapter,
+    Pagerfanta\Pagerfanta;
 
 class ListController extends Controller
 {
     /**
-     * @Route("/blog")
+     * @Route("/blog/{page}", name="posts", defaults={"page"=1})
      * @Template("JoliBlogBundle:list:list.html.twig")
      */
-    public function indexAction()
+    public function indexAction($page)
     {
-        $em = $this->get('doctrine.orm.entity_manager');
-        $posts = $em->getRepository('JoliBlogBundle:Post')->findAll();
+        //$em = $this->get('doctrine.orm.entity_manager');
+        //$posts = $em->getRepository('JoliBlogBundle:Post')->findAll();
 
-        return ['posts' => $posts];
+        $qb = $this->getDoctrine()->getEntityManager()->createQueryBuilder()
+            ->select('p')
+            ->from('Joli\BlogBundle\Entity\Post', 'p')
+            ->where('p.isPublished = :published')
+            ->setParameter('published', true);
+
+        $adapter = new DoctrineORMAdapter($qb);
+        $pagerfanta = new Pagerfanta($adapter);
+
+
+        try {
+            $entities = $pagerfanta
+                ->setMaxPerPage(10)
+                ->setCurrentPage($page)
+                ->getCurrentPageResults();
+        } catch (\Pagerfanta\Exception\NotValidCurrentPageException $e) {
+            throw $this->createNotFoundException("Cette page n'existe pas.");
+        }
+
+        return array(
+            'entities' => $entities,
+            'pager' => $pagerfanta,
+        );
+
+        // return ['posts' => $posts];
     }
 }
